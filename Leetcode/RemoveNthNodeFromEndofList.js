8b29eba2bb977fdff2d67da0b7dffdcd822b569c
_stronk <94312066+stronk-exe@users.noreply.github.com>
2023-07-27 19:31:41 +0100

    Remove Nth Node From End of List

---
+++ Leetcode/RemoveNthNodeFromEndofList.js
@@ -0,0 +1,19 @@
+var removeNthFromEnd = function(head, n) {
+    let t = []
+    let l
+
+    while (head)
+    {
+        t.push(head.val)
+        head = head.next
+    }
+    t.splice(t.length-n, 1)
+    
+    while (t.length)
+        l = new ListNode(t.pop(), l)
+    
+    if (l)
+        return l
+    return null
+};
+
