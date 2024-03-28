e75d228c33c9b7d814b0266af507edf5476beb0b
mr <70513733+stronkexe@users.noreply.github.com>
2024-03-28 15:41:24 +0000

    Delete codinGame/springChallenge_24 directory

--- codinGame/springChallenge_24/Pixel Rendering 3/PixelRendering.sql
+++
@@ -1,6 +0,0 @@
--- SQL request(s)​​​​​​‌​‌​‌‌‌​‌​​‌​​‌​‌‌​​‌​‌‌‌ below
-SELECT pixelUpdates.newColor AS color, COUNT(*) AS count FROM pixels
-INNER JOIN pixelUpdates ON pixels.id = pixelUpdates.pixelId
-WHERE pixelUpdates.updatedAt <> pixels.firstPaintedAt
-GROUP BY pixelUpdates.newColor
-ORDER BY count DESC;
